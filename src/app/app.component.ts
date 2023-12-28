db07efe9e9c242ff2d02077d32f80955827057d0
MitarKajganic <mkajganic9921rn@raf.rs>
2023-12-28 13:50:29 +0100

    dodat perm check za navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,8 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   showNavbar: boolean = true;
+  canReadUsers: boolean = false;
+  canSearchVacuum: boolean = false;
 
   constructor(private router: Router) { }
 
@@ -15,8 +18,23 @@ export class AppComponent implements OnInit {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.showNavbar = !(event.url === '/');
+        this.checkPermissions()
       }
     });
+  }
+
+  checkPermissions() {
+    const jwt = localStorage.getItem('jwt');
+    if (jwt) {
+      const decoded = jwtDecode(jwt) as any;
+      this.canReadUsers = decoded.permissions.includes('can_read_users');
+      this.canSearchVacuum = decoded.permissions.includes('can_search_vacuum');
+    } else {
+      this.canReadUsers = false;
+      this.canSearchVacuum = false;
+    }
+
+    
   }
 
   logout(): void {
